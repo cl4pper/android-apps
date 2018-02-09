c102c93f1348c800625c6f9b9217af6d9689e3e8
cl4pper <cl4pper@gmail.com>
2018-02-09 13:52:13 -0300

    Get data from input forms.

--- AlcoholOrGas/app/src/main/java/io/clapper/alcoholorgas/MainActivity.kt
+++ AlcoholOrGas/app/src/main/java/io/clapper/alcoholorgas/MainActivity.kt
@@ -2,11 +2,29 @@ package io.clapper.alcoholorgas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonResult( view: View ) {
+        // getter do preco do alcool na(da) view
+        // val priceAlcohol = findViewById<View>(R.id.edit_alcohol_price) as EditText OU (usando o plugin)
+        val priceAlcohol = edit_alcohol_price
+        val textoRecuperadoAlc = priceAlcohol.text.toString()
+        // OU val textoRecuperado = priceAlcohol.text
+
+        val priceGas = edit_gas_price
+        val textoRecuperadoGas = priceGas.text
+
+        Log.i( "RESULTADO", "texto recuperado do Alcool: $textoRecuperadoAlc" )
+        Log.i( "RESULTADO", "texto recuperado da Gasolina: $textoRecuperadoGas" )
     }
 }
