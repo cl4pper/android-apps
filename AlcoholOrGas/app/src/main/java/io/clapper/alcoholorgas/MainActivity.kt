6a321f75f8c1cc2074b4821b475fbc43801bf349
cl4pper <cl4pper@gmail.com>
2018-02-19 19:54:38 -0300

    Handling null or empty inputs.

--- AlcoholOrGas/app/src/main/java/io/clapper/alcoholorgas/MainActivity.kt
+++ AlcoholOrGas/app/src/main/java/io/clapper/alcoholorgas/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonResult( view: View ) {
+    fun buttonResult(view: View) {
         // getter do preco do alcool da view
         // val priceAlcohol = findViewById<View>(R.id.edit_alcohol_price) as EditText OU (usando o plugin)
         val priceAlcohol = edit_alcohol_price
@@ -25,17 +25,32 @@ class MainActivity : AppCompatActivity() {
 
         //Log.i( "RESULTADO", "texto recuperado do Alcool: $textoRecuperadoAlc" )
 
-        calcular( textoRecuperadoGas, textoRecuperadoAlc )
+        val validacao = validateInput( textoRecuperadoGas, textoRecuperadoAlc )
+        if ( validacao ) {
+            calcular(textoRecuperadoGas, textoRecuperadoAlc)
+        } else {
+            text_result.setText("Insira os valores.")
+        }
     }
 
-    fun calcular( priceGas: String, priceAlc: String ) {
+    fun calcular(priceGas: String, priceAlc: String) {
         val valorGas = priceGas.toDouble()
         val valorAlc = priceAlc.toDouble()
 
-        if ( valorAlc/valorGas >= 0.7 ) {
+        if (valorAlc / valorGas >= 0.7) {
             text_result.setText("Comprar Álcool.")
         } else {
             text_result.setText("Comprar Gasolina.")
         }
+    }
+
+    fun validateInput ( priceGas: String, priceAlc: String ) : Boolean {
+        var validateResult = true
+
+        if ((priceAlc == null) || (priceGas == null) || (priceAlc == "") || (priceGas == "")) {
+            validateResult = false
+        }
+
+        return validateResult
     }
 }
