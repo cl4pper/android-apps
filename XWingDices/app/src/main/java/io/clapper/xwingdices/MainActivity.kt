dc3d0e9f09a70acbf4e56d4b7ac53000c5ef174b
cl4pper <cl4pper@gmail.com>
2018-02-23 20:24:22 -0300

    Get total dices input start.

--- XWingDices/app/src/main/java/io/clapper/xwingdices/MainActivity.kt
+++ XWingDices/app/src/main/java/io/clapper/xwingdices/MainActivity.kt
@@ -2,6 +2,8 @@ package io.clapper.xwingdices
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Function to handle null input values.
+    fun nullHandler( num: Int ) : Int {
+        if( num != null ) {
+            return num
+        } else {
+            return 0
+        }
+    } // nullHandler
+
+    fun getDices() {
+        var totalDices = R.id.input_dices
+
+        println( totalDices )
+    }
+
+    fun pressButton( view: View) {
+        getDices()
+    }
+
+
 }
