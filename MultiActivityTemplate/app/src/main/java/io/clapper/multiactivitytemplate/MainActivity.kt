fc6f9c1517ffb958a87d4f4b8d94938337215aba
cl4pper <cl4pper@gmail.com>
2018-02-27 20:45:05 -0300

    Changing between Activities.

--- MultiActivityTemplate/app/src/main/java/io/clapper/multiactivitytemplate/MainActivity.kt
+++ MultiActivityTemplate/app/src/main/java/io/clapper/multiactivitytemplate/MainActivity.kt
@@ -1,12 +1,29 @@
 package io.clapper.multiactivitytemplate
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendName( view: View ) {
+
+        var nameToSend = input_name.text.toString()
+        nameToSend = "Hello, $nameToSend"
+        changeText( nameToSend )
+        //
+        val firstIntent = Intent( this, ReturnActivity::class.java )
+        startActivity( firstIntent )
+    }
+
+    fun changeText( txt: String ) {
+        output_text.setText( txt )
     }
 }
---
+++ MultiActivityTemplate/app/src/main/java/io/clapper/multiactivitytemplate/ReturnActivity.kt
@@ -0,0 +1,12 @@
+package io.clapper.multiactivitytemplate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReturnActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_return)
+    }
+}
