b2a7a4d80625427e5073aa396871c6f2d4a2cf5d
cl4pper <cl4pper@gmail.com>
2018-03-09 20:52:41 -0300

    GoT Board Game app ADD: Rounds counter.

--- GoTBoardGameApp/app/src/main/java/io/clapper/gotboardgameapp/MainActivity.kt
+++ GoTBoardGameApp/app/src/main/java/io/clapper/gotboardgameapp/MainActivity.kt
@@ -2,11 +2,49 @@ package io.clapper.gotboardgameapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //
+    }
+    // for minus button on layout
+    fun decrem( view: View ) {
+        //
+        var rounds = text_round.text.toString()
+        var finalResult = decrementAction( rounds )
+        text_round.text = finalResult
+    }
+    // for plus button on layout
+    fun increm( view: View ) {
+        //
+        var rounds = text_round.text.toString()
+        var finalResult = incrementAction( rounds )
+        text_round.text = finalResult
+    }
+    //
+    fun decrementAction( value: String ) : String {
+        var newValue = value.toInt()
+        if( value.toInt() == 1 ) {
+            return newValue.toString()
+        } else {
+            newValue --
+            return newValue.toString()
+        }
+    }
+    //
+    fun incrementAction( value: String ) : String {
+        var newValue = value.toInt()
+        newValue ++
+        return newValue .toString()
+    }
+    //
+    fun getValue( view: View ) : String {
+        var roundNumber = text_round.text.toString()
+        return roundNumber
     }
 }
