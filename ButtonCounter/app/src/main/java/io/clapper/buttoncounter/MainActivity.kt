a04928b214a3ec7e632cf2ad51c69fe56f644881
cl4pper <cl4pper@gmail.com>
2018-03-21 01:31:14 -0300

    Add: button to clear textInput> Add: textInput log> Add: reset button names log> Update: works on landscape orient.

--- ButtonCounter/app/src/main/java/io/clapper/buttoncounter/MainActivity.kt
+++ ButtonCounter/app/src/main/java/io/clapper/buttoncounter/MainActivity.kt
@@ -6,14 +6,14 @@ import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ScrollView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     // indica a classe que cada variavel pertence(dentro da view) - boas maneiras
-    private var textInput: EditText? = null
-    private var textLog: TextView? = null
+    //private var textInput: EditText? = null
+    private var logTimes: TextView? = null
+    private var logNames: TextView? = null
     //
     private var timesClicked = 0
 
@@ -21,35 +21,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //
-        textInput= findViewById<EditText>(R.id.input_text)
-        textLog = findViewById<TextView>(R.id.text_log)
-        val but_pressHere: Button = findViewById<Button>(R.id.but_pressHere)
-        val but_reset: Button = findViewById<Button>(R.id.but_reset)
-        //
-        textLog?.text = "Log:\n"
+        // Varivaeis
+        logTimes = findViewById<TextView>( R.id.log_times )
+        logNames = findViewById<TextView>( R.id.log_names )
+        val textInput: EditText = findViewById<EditText>( R.id.input_text )
+        val butAdd: Button = findViewById<Button>( R.id.but_add )
+        val butClr: Button = findViewById<Button>( R.id.but_clr )
+        val resetTimes: Button = findViewById<Button>( R.id.reset_times )
+        val resetNames: Button = findViewById<Button>( R.id.reset_names )
+
+        // atribuindo valores a variaveis
+        logTimes?.text = "LOG:\n" // .text nao e uma String, e sim uma Editable
+        logNames?.text = "NAMES:\n"
 
         // adiciona o movimento de scroll view ao widget (neste caso, ao text_log)
-        textLog?.movementMethod = ScrollingMovementMethod()
+        logTimes?.movementMethod = ScrollingMovementMethod()
+        logNames?.movementMethod = ScrollingMovementMethod()
+
         // funcao para rodar a cada click no botao
-        but_pressHere.setOnClickListener( object : View.OnClickListener {
+        butAdd.setOnClickListener( object : View.OnClickListener {
             override fun onClick(v: View?) {
+                // imprimir a quantidade de vezes do botao pressionado
                 timesClicked += 1
-                textLog?.append( "Clicked $timesClicked time" )
-                // adicionar o s dependendo da quantidade de vezes clicadas
+                logTimes?.append( "Clicked $timesClicked time" )
+                // adicionar o "s" dependendo da quantidade de vezes clicadas
                 if( timesClicked > 1 ) {
-                    textLog?.append( "s.\n" )
+                    logTimes?.append( "s.\n" )
                 } else {
-                    textLog?.append( ".\n" )
+                    logTimes?.append( ".\n" )
                 }
+                // transformar tetInput.text em uma variavel String e imprimir
+                var printingName: String? = textInput?.text.toString()
+                logNames?.append( "$printingName\n" )
             }
         })
 
-        // funcao para resetar o log e o timesClicked(= 0)
-        but_reset.setOnClickListener( object : View.OnClickListener {
+        // botao para limpar caixa de texto
+        butClr.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                textInput?.text.clear()
+            }
+        })
+
+        // funcao para resetar o log das vezes clicadas
+        resetTimes.setOnClickListener( object : View.OnClickListener {
             override fun onClick( v: View? ) {
                 timesClicked = 0
-                textLog?.text = "Log:\n"
+                logTimes?.text = "LOG:\n"
+            }
+        })
+
+        // funcao para resetar o log dos nomes inseridos
+        resetNames.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                logNames?.text = "NAMES:\n"
             }
         })
     }
