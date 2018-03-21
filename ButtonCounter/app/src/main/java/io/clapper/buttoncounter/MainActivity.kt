6e63196273b9cec6924abb86635a8ac2106bdfe4
cl4pper <cl4pper@gmail.com>
2018-03-21 00:39:28 -0300

    Remove: scroll view for log. Update: scrolling movement for text log in code.

--- ButtonCounter/app/src/main/java/io/clapper/buttoncounter/MainActivity.kt
+++ ButtonCounter/app/src/main/java/io/clapper/buttoncounter/MainActivity.kt
@@ -2,18 +2,18 @@ package io.clapper.buttoncounter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ScrollView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     // indica a classe que cada variavel pertence(dentro da view) - boas maneiras
-    private var text_input: EditText? = null
-    private var text_log: TextView? = null
-    private var but_pressHere: Button? = null
-    private var but_reset: Button? = null
+    private var textInput: EditText? = null
+    private var textLog: TextView? = null
     //
     private var timesClicked = 0
 
@@ -22,24 +22,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //
-        text_input = findViewById<EditText>(R.id.input_text)
-        text_log = findViewById<TextView>(R.id.text_log)
-        but_pressHere = findViewById<Button>(R.id.but_pressHere)
-        but_reset = findViewById<Button>(R.id.but_reset)
+        textInput= findViewById<EditText>(R.id.input_text)
+        textLog = findViewById<TextView>(R.id.text_log)
+        val but_pressHere: Button = findViewById<Button>(R.id.but_pressHere)
+        val but_reset: Button = findViewById<Button>(R.id.but_reset)
+        //
+        textLog?.text = "Log:\n"
 
+        // adiciona o movimento de scroll view ao widget (neste caso, ao text_log)
+        textLog?.movementMethod = ScrollingMovementMethod()
         // funcao para rodar a cada click no botao
-        but_pressHere?.setOnClickListener(object : View.OnClickListener {
+        but_pressHere.setOnClickListener( object : View.OnClickListener {
             override fun onClick(v: View?) {
                 timesClicked += 1
-                text_log?.append("\nThe button has been clicked $timesClicked times.")
+                textLog?.append( "Clicked $timesClicked time" )
+                // adicionar o s dependendo da quantidade de vezes clicadas
+                if( timesClicked > 1 ) {
+                    textLog?.append( "s.\n" )
+                } else {
+                    textLog?.append( ".\n" )
+                }
             }
         })
 
         // funcao para resetar o log e o timesClicked(= 0)
-        but_reset?.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
+        but_reset.setOnClickListener( object : View.OnClickListener {
+            override fun onClick( v: View? ) {
                 timesClicked = 0
-                text_log?.text = "Log starts down here:"
+                textLog?.text = "Log:\n"
             }
         })
     }
