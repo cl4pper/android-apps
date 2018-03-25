0554b02e088b2b65cea45d8bf15c8cf7f797e2bd
cl4pper <cl4pper@gmail.com>
2018-03-25 15:35:43 -0300

    TechTree: Add: connecting mainActivity with calculatorActivity. Update: when clicked buttons change. Update: buttons back to normal state when moves to another activity.

--- TechThree/app/src/main/java/io/clapper/techthree/CalculatorActivity.kt
+++ TechThree/app/src/main/java/io/clapper/techthree/CalculatorActivity.kt
@@ -1,12 +1,35 @@
 package io.clapper.techthree
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class CalculatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        val backButton: ImageView = findViewById( R.id.but_back )
+
+        // intent da activity principal
+        val introActivity = Intent( this, MainActivity::class.java )
+
+        // metodo do botao para retornar a activity inicial
+        backButton.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                backButton.setImageResource( R.drawable.ic_back_pressed_80dp )
+                startActivity( introActivity )
+            }
+        })
+    }
+
+    // para fazer o botao retornar ao estado original - antes do verde
+    override fun onStop() {
+        super.onStop()
+        val backButton: ImageView = findViewById( R.id.but_back )
+        backButton.setImageResource( R.drawable.ic_back_80dp )
     }
 }
--- TechThree/app/src/main/java/io/clapper/techthree/MainActivity.kt
+++ TechThree/app/src/main/java/io/clapper/techthree/MainActivity.kt
@@ -1,12 +1,37 @@
 package io.clapper.techthree
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nextButton: ImageView = findViewById( R.id.but_next )
+
+        // intent para a activity da calculadora
+        val calculatorActivity = Intent( this, CalculatorActivity::class.java )
+
+        // metodo do botao avancar para proxima activity
+        nextButton.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                nextButton.setImageResource( R.drawable.ic_pressed_80dp )
+                startActivity( calculatorActivity )
+            }
+        })
+    }
+
+    // para fazer o botao retornar ao estado original - antes do verde
+    override fun onStop() {
+        super.onStop()
+        //Toast.makeText( applicationContext, "onStop", Toast.LENGTH_LONG ).show()
+        val nextButton: ImageView = findViewById( R.id.but_next )
+        nextButton.setImageResource( R.drawable.ic_next_24dp )
     }
 }
