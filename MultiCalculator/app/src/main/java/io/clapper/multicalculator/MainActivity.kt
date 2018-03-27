3fb8cd0eab1d04e4ca95d5849d385b051b552c89
cl4pper <cl4pper@gmail.com>
2018-03-27 00:48:53 -0300

    MultiCalculator: Add: buttons pressed/released state XML drawable Update: layout constraints

--- MultiCalculator/app/src/main/java/io/clapper/multicalculator/MainActivity.kt
+++ MultiCalculator/app/src/main/java/io/clapper/multicalculator/MainActivity.kt
@@ -1,12 +1,35 @@
 package io.clapper.multicalculator
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val button: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // instancia dos botoes
+        val zero: Button = findViewById( R.id.but_0 )
+        val one: Button = findViewById( R.id.but_1 )
+
+        /*
+        zero.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                zero.setBackgroundColor( Color.parseColor( "#ffffff" ) )
+            }
+        })
+
+        one.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                one.setBackgroundColor( Color.parseColor( "#ffffff" ) )
+            }
+        })
+        */
     }
 }
