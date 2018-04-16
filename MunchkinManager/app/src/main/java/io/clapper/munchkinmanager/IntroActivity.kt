c7e7f13ac88b140a24f94d7d6bad1355cb30cb0a
cl4pper <cl4pper@gmail.com>
2018-04-16 16:48:39 -0300

    Munchkin Manager: Update: Intro layout done, MainActivity interaction code.

--- MunchkinManager/app/src/main/java/io/clapper/munchkinmanager/IntroActivity.kt
+++ MunchkinManager/app/src/main/java/io/clapper/munchkinmanager/IntroActivity.kt
@@ -1,12 +1,26 @@
 package io.clapper.munchkinmanager
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val start: Button = findViewById( R.id.but_start )
+
+        val MainView = Intent( this, MainActivity::class.java )
+
+        start.setOnClickListener( object : View.OnClickListener {
+            override fun onClick( v: View? ) {
+                // vai para a tela de numero de jogadores
+                startActivity( MainView )
+            }
+        })
     }
 }
--- MunchkinManager/app/src/main/java/io/clapper/munchkinmanager/MainActivity.kt
+++ MunchkinManager/app/src/main/java/io/clapper/munchkinmanager/MainActivity.kt
@@ -2,11 +2,36 @@ package io.clapper.munchkinmanager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adicionar: Button = findViewById( R.id.but_add )
+        val remover: Button = findViewById( R.id.but_remove )
+        val quantJogadores: TextView = findViewById( R.id.text_quant )
+
+        adicionar.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //
+                var numero = quantJogadores.toString().toInt()
+                numero++
+                quantJogadores.text = numero.toString()
+            }
+        })
+
+        remover.setOnClickListener( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //
+                var numero = quantJogadores.toString().toInt()
+                numero--
+                quantJogadores.text = numero.toString()
+            }
+        })
     }
 }
